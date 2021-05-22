eca7e06154e0400bb75b49d60e1ccf686435151d
RadGoodNowYT <81907382+RadGoodNowYT@users.noreply.github.com>
2021-05-22 14:08:46 +0400

    Add files via upload

--- unit1.pas
+++ unit1.pas
@@ -18,8 +18,11 @@ type
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
   private
 
   public
@@ -44,6 +47,11 @@ procedure TForm1.MenuItem3Click(Sender: TObject);
 begin
   ShowMessage('Dont open me anymore!');
   close
+end;
+
+procedure TForm1.MenuItem5Click(Sender: TObject);
+begin
+  ShowMessage('No')
 end;
 
 end.
