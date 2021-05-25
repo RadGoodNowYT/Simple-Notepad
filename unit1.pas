bf13f1039c5c27a4f0cc4ff071960c5e961db146
RadGoodNowYT <81907382+RadGoodNowYT@users.noreply.github.com>
2021-05-25 15:40:00 +0400

    Add files via upload

--- unit1.pas
+++ unit1.pas
@@ -1,7 +1,9 @@
+ //Start of code
 unit Unit1;
 
 {$mode objfpc}{$H+}
 
+//what will use
 interface
 
 uses
@@ -12,14 +14,22 @@ type
 
   { TForm1 }
 
+//class
   TForm1 = class(TForm)
     MainMenu1: TMainMenu;
     Here: TMemo;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
     procedure MenuItem2Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
+    procedure MenuItem6Click(Sender: TObject);
   private
 
   public
@@ -35,15 +45,29 @@ implementation
 
 { TForm1 }
 
+
+//buttons
 procedure TForm1.MenuItem2Click(Sender: TObject);
 begin
-  ShowMessage('I dont will save your writed shit!')
+ if SaveDialog1.execute then
+ Here.Lines.SaveToFile(SaveDialog1.filename)
 end;
 
 procedure TForm1.MenuItem3Click(Sender: TObject);
 begin
   ShowMessage('Dont open me anymore!');
   close
+end;
+
+procedure TForm1.MenuItem5Click(Sender: TObject);
+begin
+  ShowMessage('No')
+end;
+
+procedure TForm1.MenuItem6Click(Sender: TObject);
+begin
+  if OpenDialog1.execute then
+ Here.Lines.LoadFromFile(OpenDialog1.filename)
 end;
 
 end.
